8d2be4e5c3ad7b40e386ceac2c6cb576a0bbab9b
Dillon Drenzek <dr.enzek@gmail.com>
2016-05-14 18:45:56 -0700

    close #2 - angular init

---
+++ app/App.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app',
+  template: '<h1>Spotify-Timeline</h1>',
+  styles: [
+	  `
+	  h1 {
+		  font-family: helvetica;
+	  }
+	  `
+  ]
+})
+export class App { }
---
+++ app/boot.ts
@@ -0,0 +1,3 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { App } from './App';
+bootstrap(App);
