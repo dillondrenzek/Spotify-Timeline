83cf5904431fed56dbf09b1429dd3b959a1a5fe9
Dillon Drenzek <dr.enzek@gmail.com>
2016-11-12 13:52:18 -0800

    reduce SpotifyDataService

--- public/app/spotify-data/spotifyDataService/spotifyData.service.ts
+++ public/app/spotify-data/spotifyDataService/spotifyData.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Request, RequestMethod, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class SpotifyDataService {
 
   constructor(private http: Http) {}
-
-  // getAppVersion(): Observable<string> {
-  //   // configure HTTP request
-  //   var req = new Request({
-  //     method: RequestMethod.Get,
-  //     url: '/meta/version'
-  //   });
-  //
-  //   // make HTTP request
-  //   return this.http
-  //     .request(req)
-  //     .map((res: Response) => {
-  //       let packageObj = res.json();
-  //       return packageObj['version'];
-  //     });
-  // }
 
 }
