907bb3f8a383f24b4c25db4a8ad4f3c910ac38e1
Dillon Drenzek <dillon@drenzek.com>
2020-07-22 23:09:08 -0700

    first step of login process is working

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
+import { parse } from 'path';
 
 interface AppEnvironment {
   APP_PORT: number;
   SPOTIFY_API_CLIENT_ID: string;
   SPOTIFY_API_CLIENT_SECRET: string;
+  SPOTIFY_API_REDIRECT_URI: string;
 }
 
 export default function(): AppEnvironment {
@@ -17,6 +19,7 @@ export default function(): AppEnvironment {
   return {
     APP_PORT: parseInt(parsed.APP_PORT, 10),
     SPOTIFY_API_CLIENT_ID: parsed.SPOTIFY_API_CLIENT_ID,
-    SPOTIFY_API_CLIENT_SECRET: parsed.SPOTIFY_API_CLIENT_SECRET
+    SPOTIFY_API_CLIENT_SECRET: parsed.SPOTIFY_API_CLIENT_SECRET,
+    SPOTIFY_API_REDIRECT_URI: parsed.SPOTIFY_API_REDIRECT_URI
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,23 @@ const port = env.APP_PORT; // default port to listen
 
 app.use('/api', api);
 
+app.get('/spotify/login', (req, res) => {
+  const scope = ['user-read-private', 'user-read-email'].join(' ');
+  const params = new URLSearchParams([
+    ['client_id', env.SPOTIFY_API_CLIENT_ID],
+    ['response_type', 'code'],
+    ['redirect_uri', env.SPOTIFY_API_REDIRECT_URI],
+    ['scope', scope]
+  ]).toString();
+  const url = `https://accounts.spotify.com/authorize?${params}`;
+  res.redirect(url);
+});
+
+app.get('/spotify/callback', (req, res) => {
+  console.log('spotify callback', req.query);
+  res.send('Ok');
+});
+
 // define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send('Hello world!');
