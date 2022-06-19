7cc002786bf4a8d57710d9468c3ecf567c425c40
Dillon Drenzek <dillon@drenzek.com>
2022-06-18 22:23:10 -0700

    add CssBaseline

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, CssBaseline } from '@mui/material';
 import { Nav } from './app/Nav';
 import './App.scss';
 import { SavedTracksTable } from './app/saved-tracks-table';
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Box sx={{ mt: 8 }}>
-        <Typography variant="h2">Saved Tracks</Typography>
-        <SavedTracksTable />
-      </Box>
-    </Container>
+    <>
+      <CssBaseline />
+      <Container>
+        <Nav />
+        <Box sx={{ mt: 8 }}>
+          <Typography variant="h2">Saved Tracks</Typography>
+          <SavedTracksTable />
+        </Box>
+      </Container>
+    </>
   );
 }
 
