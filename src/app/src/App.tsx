c7be2e9015179c8c89864d8a75848d73fa4ea030
Dillon Drenzek <dillon@drenzek.com>
2022-06-18 21:33:50 -0700

    move App

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
-import { Nav } from './nav/Nav';
+import { Nav } from './components/nav/Nav';
 import './App.scss';
-import { SavedTracksTable } from './saved-tracks/saved-tracks-table';
+import { SavedTracksTable } from './components/saved-tracks/saved-tracks-table';
 
 function App() {
-  
   return (
     <Container>
       <Nav />
       <Box sx={{ mt: 8 }}>
-        <Typography variant='h2'>Saved Tracks</Typography>
-      <SavedTracksTable />
+        <Typography variant="h2">Saved Tracks</Typography>
+        <SavedTracksTable />
       </Box>
     </Container>
   );
