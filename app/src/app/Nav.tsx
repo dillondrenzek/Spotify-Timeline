3e2730c7962114dc8fc2a461158d7db45ce42945
Dillon Drenzek <dillon@drenzek.com>
2022-06-18 22:13:16 -0700

    app is running again

--- app/src/app/Nav.tsx
+++ app/src/app/Nav.tsx
@@ -1,36 +1,38 @@
 import React, { PropsWithChildren } from 'react';
 import { AppBar, Box, Toolbar, Typography, Link, Stack } from '@mui/material';
-import { useCurrentUser } from '../../hooks/use-current-user';
+import { useCurrentUser } from '../hooks/use-current-user';
 import './Nav.scss';
 
-function NavProfileDisplay(props: PropsWithChildren<{
-  user: SpotifyApi.CurrentUserProfile
-}>) {
+function NavProfileDisplay(
+  props: PropsWithChildren<{
+    user: SpotifyApi.CurrentUserProfile;
+  }>
+) {
   const { user } = props;
   return (
-    <Stack direction='row' spacing={1} alignItems='center'>
+    <Stack direction="row" spacing={1} alignItems="center">
       {user.images?.length ? (
-        <img
-          alt="User's profile"
-          src={user.images[0].url}
-          height={48}
-        />
+        <img alt="User's profile" src={user.images[0].url} height={48} />
       ) : null}
-      <Stack direction='column' spacing={0}>
+      <Stack direction="column" spacing={0}>
         <Typography>{user?.display_name}</Typography>
         <Typography>{user?.email}</Typography>
       </Stack>
     </Stack>
-  )
+  );
 }
 
-function NavLink(props: PropsWithChildren<{
-  href: string;
-}>) {
+function NavLink(
+  props: PropsWithChildren<{
+    href: string;
+  }>
+) {
   const { children, ...passthrough } = props;
   return (
     <Box>
-      <Link color='inherit' {...passthrough}>{children}</Link>
+      <Link color="inherit" {...passthrough}>
+        {children}
+      </Link>
     </Box>
   );
 }
@@ -39,20 +41,20 @@ export function Nav() {
   const { currentUser } = useCurrentUser();
 
   return (
-    <AppBar color='default'>
-      <Toolbar sx={{ justifyContent: 'space-between'}}>
-        <Stack direction='row' spacing={3} alignItems='center'>
-          <Typography 
-            variant='h6'
-            noWrap
-            component='div'
-          >Spotify Timeline</Typography>
+    <AppBar color="default">
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Stack direction="row" spacing={3} alignItems="center">
+          <Typography variant="h6" noWrap component="div">
+            Spotify Timeline
+          </Typography>
           <NavLink href="/">Home</NavLink>
         </Stack>
         {currentUser ? (
-          <Stack direction='row' spacing={1} alignItems='center'>
-            <NavProfileDisplay user={currentUser} /> 
-            <NavLink href="https://accounts.spotify.com/en/status">Account</NavLink>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <NavProfileDisplay user={currentUser} />
+            <NavLink href="https://accounts.spotify.com/en/status">
+              Account
+            </NavLink>
             <NavLink href="/spotify/logout">Logout</NavLink>
           </Stack>
         ) : (
@@ -61,4 +63,4 @@ export function Nav() {
       </Toolbar>
     </AppBar>
   );
-};
+}
