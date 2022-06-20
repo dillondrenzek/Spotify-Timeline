d4acbd3111d074353e8a4b89c04308b6d3699b9a
Dillon Drenzek <dillon@drenzek.com>
2022-06-19 17:23:44 -0700

    add playlist tracks api endpint; fix status exception

--- server/api/index.ts
+++ server/api/index.ts
@@ -11,6 +11,22 @@ export default function (spotifyWebApi: SpotifyWebApi) {
 
   api.use(cookieParser());
 
+  api.get('/playlists/:id/tracks', async (req, res) => {
+    const { params } = req;
+    const playlistId = params.id;
+
+    try {
+      const tracks = await spotifyWebApi.getPlaylistItems(
+        playlistId,
+        getAccessToken(req)
+      );
+      res.json(tracks);
+    } catch (e) {
+      res.status(e.error?.status ?? 500);
+      res.json(e.error);
+    }
+  });
+
   api.get('/me/playlists', async (req, res) => {
     try {
       const playlists = await spotifyWebApi.getUsersPlaylists(
@@ -18,7 +34,7 @@ export default function (spotifyWebApi: SpotifyWebApi) {
       );
       res.json(playlists.items);
     } catch (e) {
-      res.status(e.error.status);
+      res.status(e.error?.status ?? 500);
       res.json(e.error);
     }
   });
@@ -28,7 +44,7 @@ export default function (spotifyWebApi: SpotifyWebApi) {
       const user = await spotifyWebApi.getUsersSavedTracks(getAccessToken(req));
       res.json(user);
     } catch (e) {
-      res.status(e.error.status);
+      res.status(e.error?.status ?? 500);
       res.json(e.error);
     }
   });
@@ -38,7 +54,7 @@ export default function (spotifyWebApi: SpotifyWebApi) {
       const user = await spotifyWebApi.getMe(getAccessToken(req));
       res.json(user);
     } catch (e) {
-      res.status(e.error.status);
+      res.status(e.error?.status ?? 500);
       res.json(e.error);
     }
   });
