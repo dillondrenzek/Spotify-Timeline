62e2d5ff65927306b7fbef806d01347f860a3879
Dillon Drenzek <dillon@drenzek.com>
2022-07-04 01:06:11 -0700

    add more info to playbar

--- app/src/app/playbar.tsx
+++ app/src/app/playbar.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
 import { AppBar, Toolbar, Stack, Typography, Button, Box } from '@mui/material';
-import { usePlayerState } from '../hooks/use-player-state';
+import { usePlayerState, PlayerState } from '../hooks/use-player-state';
 import { green, grey, orange } from '@mui/material/colors';
+
+function DeviceDisplay(props: {
+  device: PlayerState['device'];
+  isPlaying: boolean;
+}) {
+  const { device, isPlaying } = props;
+
+  if (!device) {
+    return null;
+  }
+
+  return (
+    <Stack direction="column">
+      <Typography
+        variant="caption"
+        fontWeight="bold"
+        color={isPlaying ? green[400] : orange[700]}
+      >
+        {isPlaying ? 'Playing' : 'Paused'} on
+      </Typography>
+      <Typography variant="caption">{device?.name}</Typography>
+    </Stack>
+  );
+}
+
+function CurrentItemDisplay(props: { item: PlayerState['item'] }) {
+  const { item } = props;
+
+  return item ? (
+    <Stack direction="column">
+      <Typography>{item?.name || ''}</Typography>
+      <Box>
+        {Array.isArray(item?.artists)
+          ? item?.artists.map((item, i) => (
+              <Typography
+                variant="caption"
+                key={i}
+                sx={{ color: grey[400], mr: 1 }}
+              >
+                {item?.name}
+              </Typography>
+            ))
+          : ''}
+      </Box>
+    </Stack>
+  ) : null;
+}
 
 export function Playbar() {
   const { state, fetch, timestamp } = usePlayerState();
-  const { item, is_playing } = state;
+  const { item, is_playing, device, repeat_state, shuffle_state } = state;
 
   return (
     <AppBar color="default" position="fixed" sx={{ top: 'auto', bottom: 0 }}>
@@ -22,31 +69,16 @@ export function Playbar() {
               </Typography>
             ) : null}
           </Stack>
-          <Typography
-            variant="caption"
-            fontWeight="bold"
-            color={is_playing ? green[400] : orange[700]}
-          >
-            {is_playing ? 'Playing' : 'Paused'}
-          </Typography>
-          {item ? (
-            <Stack direction="column">
-              <Typography>{item?.name || 'Untitled'}</Typography>
-              <Box>
-                {Array.isArray(item?.artists)
-                  ? item?.artists.map((item, i) => (
-                      <Typography
-                        variant="caption"
-                        key={i}
-                        sx={{ color: grey[400], mr: 1 }}
-                      >
-                        {item?.name}
-                      </Typography>
-                    ))
-                  : ''}
-              </Box>
-            </Stack>
-          ) : null}
+          <DeviceDisplay device={device} isPlaying={is_playing} />
+          <Stack direction="column">
+            <Typography variant="caption" sx={{ color: grey[400], mr: 1 }}>
+              Repeat: {repeat_state}
+            </Typography>
+            <Typography variant="caption" sx={{ color: grey[400], mr: 1 }}>
+              Shuffle: {shuffle_state ? 'on' : 'off'}
+            </Typography>
+          </Stack>
+          <CurrentItemDisplay item={item} />
         </Stack>
       </Toolbar>
     </AppBar>
--- app/src/hooks/use-player-state.ts
+++ app/src/hooks/use-player-state.ts
@@ -1,69 +1,14 @@
 // import * as Types from '../lib/timeline';
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import { httpRequest, responseParser } from '../lib/http';
+import { PlayerStateResult } from '../lib/player/player-types';
 // import { useAuthToken } from './use-auth-token';
 // import { httpRequest, responseParser } from '../lib/http';
 
 /**
- * The response from the Spotify Web API
- */
-export interface PlayerStateResult {
-  // device
-  // object
-  // The device that is currently active.
-  device: Record<string, unknown>;
-
-  // repeat_state
-  // string
-  // off, track, context
-  repeat_state: 'off' | 'track' | 'context';
-
-  // shuffle_state
-  // string
-  // If shuffle is on or off.
-  shuffle_state: string;
-
-  // context
-  // object
-  // A Context Object. Can be null.
-  context: Record<string, unknown>;
-
-  // timestamp
-  // integer
-  // Unix Millisecond Timestamp when data was fetched.
-  timestamp: number;
-
-  // progress_ms
-  // integer
-  // Progress into the currently playing track or episode. Can be null.
-  progress_ms: number;
-
-  // is_playing
-  // boolean
-  // If something is currently playing, return true.
-  is_playing: boolean;
-
-  /**
-   * Item currently playing
-   */
-  item: Record<string, unknown>;
-
-  // object
-  // currently_playing_type
-  // string
-  // The object type of the currently playing item. Can be one of track, episode, ad or unknown.
-  currently_playing_type: 'track' | 'episode' | 'ad' | 'unknown';
-
-  // actions
-  // object
-  // Allows to update the user interface based on which playback actions are available within the current context.
-  actions: Record<string, unknown>;
-}
-
-/**
  * Application Player state
  */
-type PlayerState = PlayerStateResult;
+export type PlayerState = PlayerStateResult;
 
 const defaultPlayerState: PlayerState = {
   actions: null,
