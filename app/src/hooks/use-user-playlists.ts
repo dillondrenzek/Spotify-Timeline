d2a59e53bd5276ddd197925f0d1f750e4ee50a13
Dillon Drenzek <dillon@drenzek.com>
2023-01-14 20:52:05 -0800

    update useUserPlaylists

--- app/src/hooks/use-user-playlists.ts
+++ app/src/hooks/use-user-playlists.ts
@@ -1,32 +1,47 @@
 import { useState, useEffect } from 'react';
+import { ErrorHandler } from '../lib/error';
+import { httpRequest, parseResponse } from '../lib/http';
 import { useAuthToken } from './use-auth-token';
 
-export function useUserPlaylists() {
+function isValidResult(
+  value: unknown
+): value is SpotifyApi.CurrentUserPlaylist[] {
+  if (!Array.isArray(value)) {
+    return false;
+  }
+
+  // TODO: check for specific properties
+
+  return true;
+}
+
+function convert(
+  result: SpotifyApi.CurrentUserPlaylist[]
+): SpotifyApi.CurrentUserPlaylist[] {
+  return result;
+}
+
+export function useUserPlaylists(handleError?: ErrorHandler) {
   const { authToken, clearAuthToken } = useAuthToken();
   const [playlists, setPlaylists] = useState<SpotifyApi.CurrentUserPlaylist[]>(
     []
   );
+  const [requestMade, setRequestMade] = useState(false);
 
   useEffect(() => {
-    if (authToken && !playlists?.length) {
-      fetch('/api/me/playlists')
-        .then((res) => {
-          res
-            .json()
-            .then((result: SpotifyApi.CurrentUserPlaylist[]) => {
-              console.log('result:', result);
-              setPlaylists(result);
-            })
-            .catch((err) => {
-              console.error('Error parsing JSON:', err);
-            });
+    if (authToken && !requestMade) {
+      httpRequest('/api/me/playlists')
+        .then(parseResponse(isValidResult, convert))
+        .then((playlists) => {
+          setPlaylists(playlists);
         })
         .catch((err) => {
           console.error('Error fetching /api/me:', err);
-          // clearAuthToken();
+          handleError?.(err);
         });
+      setRequestMade(true);
     }
-  }, [authToken, clearAuthToken, playlists]);
+  }, [authToken, clearAuthToken, requestMade, handleError]);
 
   return {
     playlists,
