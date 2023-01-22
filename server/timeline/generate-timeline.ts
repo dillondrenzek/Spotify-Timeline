47dd1e2d1ac09684ae2da9e75e0a2bfbc62ba92c
Dillon Drenzek <dillon@drenzek.com>
2023-01-21 16:31:26 -0800

    new grouping algo seems to be working

--- server/timeline/generate-timeline.ts
+++ server/timeline/generate-timeline.ts
@@ -1,5 +1,7 @@
 import * as SpotifyTypes from '../spotify/types';
 import express from 'express';
+import { ckmeans } from 'simple-statistics';
+import { DateTime } from 'luxon';
 import { SpotifyConverter } from './spotify-converter';
 import * as Types from './timeline-types';
 import { chunk, reduce } from 'lodash';
@@ -70,6 +72,93 @@ async function fetchSavedTracks(
   return result;
 }
 
+function normalizeDate(
+  subject: DateTime,
+  minDate: DateTime,
+  maxDate: DateTime
+): number {
+  const minNormalizedValue = 0;
+  const maxNormalizedValue = 1;
+
+  // TODO: ensure subject is between `minDate` `maxDate`
+
+  const minMs = minDate.toMillis();
+  const maxMs = maxDate.toMillis();
+  const subjectMs = subject.toMillis();
+
+  return (subjectMs - minMs) / (maxMs - minMs);
+}
+
+function getMinMaxDate(data: SpotifyTypes.SavedTrack[]): [DateTime, DateTime] {
+  const dates = data.map((d) => {
+    return DateTime.fromISO(d.added_at);
+  });
+  return [DateTime.min(...dates), DateTime.max(...dates)];
+}
+
+/**
+ *
+ * @param tracks
+ * @param options
+ * @returns
+ */
+function groupTracks(
+  tracks: SpotifyTypes.SavedTrack[],
+  options: GenerateTimelineOptions
+): SpotifyTypes.SavedTrack[][] {
+  // Timeline Options
+  const { numPlaylists } = options;
+
+  // Calculate Min Date and Max Date
+  const [minDate, maxDate] = getMinMaxDate(tracks);
+
+  // Normalize Date Added values to (0-1 value)
+  const normalizedTracks: {
+    [normalizedValue: number]: SpotifyTypes.SavedTrack;
+  } = {};
+  const normalizedValues: number[] = [];
+
+  // Add Saved Track to map
+  tracks.forEach((track) => {
+    // Normalize added_at Date as a number between 0-1
+    const normalizedValue = normalizeDate(
+      DateTime.fromISO(track.added_at),
+      minDate,
+      maxDate
+    );
+    normalizedValues.push(normalizedValue);
+
+    // Map Track to it's corresponding normalized value
+    normalizedTracks[normalizedValue] = track;
+  });
+
+  // Run ckmeans algorithm
+  const groupedNormalizedValues = ckmeans(normalizedValues, numPlaylists);
+
+  // Map ckmeans result back to SavedTrack[][];
+  const groupedSavedTracks: SpotifyTypes.SavedTrack[][] =
+    groupedNormalizedValues.map((group: number[]) => {
+      return group.map((normalizedValue: number) => {
+        return normalizedTracks[normalizedValue];
+      });
+    });
+
+  return groupedSavedTracks;
+}
+
+function convertGroupsToPlaylists(
+  groups: SpotifyTypes.SavedTrack[][]
+): Types.SuggestedPlaylist[] {
+  return groups.map((group, i) => {
+    const [minDate, maxDate] = getMinMaxDate(group);
+    const dateFormat = 'yyyy LLL dd';
+    const playlistName = `Playlist ${i.toString()} - (${minDate.toFormat(
+      dateFormat
+    )} - ${maxDate.toFormat(dateFormat)})`;
+    return suggestedPlaylist(playlistName, group);
+  });
+}
+
 export async function generateTimeline(
   spotifyWebApi: SpotifyWebApi,
   accessToken: string,
@@ -86,13 +175,11 @@ export async function generateTimeline(
   );
 
   // Group Tracks into playlists
-  const tracksGroupedToSize = chunk(savedTracks, groupSize);
+  const tracksGroupedToSize = groupTracks(savedTracks, options);
 
   // Create suggested playlists
-  const suggestedPlaylists: Types.SuggestedPlaylist[] = tracksGroupedToSize.map(
-    (groupedTracks, i) =>
-      suggestedPlaylist('Playlist ' + i.toString(), groupedTracks)
-  );
+  const suggestedPlaylists: Types.SuggestedPlaylist[] =
+    convertGroupsToPlaylists(tracksGroupedToSize);
 
   return {
     suggestedPlaylists,
