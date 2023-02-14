09d5bc508ee77a6fadb35a1f3eff7ea4a0793ef1
Dillon Drenzek <dillon@drenzek.com>
2023-02-13 21:04:12 -0800

    move to suggested playlist

---
+++ server/timeline/suggested-playlist.ts
@@ -0,0 +1,43 @@
+import * as SpotifyTypes from '../spotify/types';
+import { formatDate } from '../lib/date-time';
+import { SpotifyConverter } from './spotify-converter';
+import { ApiTypes } from 'api-types';
+import { getMinMaxDate } from './generate-timeline';
+
+/**
+ * Counts the number of Tracks in a group of items
+ */
+export function countTracks(input: ApiTypes.SuggestedPlaylist[]): number {
+  return input
+    .map((pl) => pl.tracks.length)
+    .reduce((prev, curr) => prev + curr, 0);
+}
+
+export function suggestedPlaylist(
+  savedTracks: SpotifyTypes.SavedTrack[] = []
+): ApiTypes.SuggestedPlaylist {
+  const [minDate, maxDate] = getMinMaxDate(savedTracks);
+  const startDate = minDate?.toISODate() ?? null;
+  const endDate = maxDate?.toISODate() ?? null;
+
+  const title = [
+    'Liked Songs',
+    minDate && maxDate
+      ? `(${formatDate(minDate, 'yyyy LLL dd')} - ${formatDate(
+          maxDate,
+          'yyyy LLL dd'
+        )})`
+      : null,
+  ]
+    .filter(Boolean)
+    .join(' - ');
+
+  const tracks = savedTracks.map((t) => SpotifyConverter.toTrack(t)).reverse();
+
+  return {
+    startDate,
+    endDate,
+    title,
+    tracks,
+  };
+}
