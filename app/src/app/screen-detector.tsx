04e06fedae44d32e1f7fc4ed06eb04d54e6db3ca
Dillon Drenzek <dillon@drenzek.com>
2023-02-20 14:43:04 -0800

    move useOnScreen

--- app/src/app/screen-detector.tsx
+++ app/src/app/screen-detector.tsx
@@ -1,47 +1,14 @@
-import React, {
-  MutableRefObject,
-  ReactNode,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
 import { Box } from '@mui/material';
+import { useOnScreen } from '../hooks/use-on-screen';
 
-// Hook
-function useOnScreen<T extends Element>(
-  ref: MutableRefObject<T>,
-  rootMargin: string = '0px'
-): boolean {
-  // State and setter for storing whether element is visible
-  const [isIntersecting, setIntersecting] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        // Update our state when observer callback fires
-        setIntersecting(entry.isIntersecting);
-      },
-      {
-        rootMargin,
-        // threshold: 1.0,
-      }
-    );
-    const currentRef = ref.current;
-    if (currentRef) {
-      observer.observe(currentRef);
-    }
-    return () => {
-      observer.unobserve(currentRef);
-    };
-  }, [rootMargin, ref]); // Empty array ensures that effect is only run on mount and unmount
-  return isIntersecting;
-}
-
-export function ScreenDetector(props: {
+interface ScreenDetectorProps {
   children?: ReactNode;
   onEnterScreen?: () => void;
   onExitScreen?: () => void;
-}) {
+}
+
+export function ScreenDetector(props: ScreenDetectorProps) {
   const { children, onEnterScreen, onExitScreen } = props;
   const ref = useRef();
 
