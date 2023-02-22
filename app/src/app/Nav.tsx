7199397f8a35d948f3b4f4d9a7259faa01bf5db1
Dillon Drenzek <dillon@drenzek.com>
2023-02-21 23:06:09 -0800

    closes #102 - profile avatar

--- app/src/app/Nav.tsx
+++ app/src/app/Nav.tsx
@@ -1,4 +1,9 @@
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+} from 'react';
 import {
   AppBar,
   Box,
@@ -7,10 +12,17 @@ import {
   Link,
   Stack,
   LinkProps,
+  Avatar,
+  AvatarProps,
+  IconButton,
+  Menu,
+  MenuItem,
+  Divider,
 } from '@mui/material';
 import { AuthLinks } from '../lib/auth';
 import { useUserStore } from '../stores/use-user-store';
 import { ApiTypes } from 'api-types';
+import { useToggle } from 'usehooks-ts';
 
 export function Nav() {
   const { currentUser, isAuthenticated, isLoaded, pullCurrentUser, logout } =
@@ -33,11 +45,11 @@ export function Nav() {
           {currentUser ? <NavLink href="/timeline">Timeline</NavLink> : null}
         </Stack>
         {currentUser ? (
-          <Stack direction="row" spacing={3} alignItems="center">
-            <NavProfileDisplay user={currentUser} />
-            <NavLink onClick={logout}>Logout</NavLink>
-          </Stack>
+          <NavProfileDisplay user={currentUser} />
         ) : (
+          // <Stack direction="row" spacing={3} alignItems="center">
+          //   <NavLink onClick={logout}>Logout</NavLink>
+          // </Stack>
           <NavLink href={AuthLinks.login}>Login</NavLink>
         )}
       </Toolbar>
@@ -45,21 +57,82 @@ export function Nav() {
   );
 }
 
+/**
+ * NOTE: `user` is guaranteed
+ */
 function NavProfileDisplay(
   props: PropsWithChildren<{
     user: ApiTypes.CurrentUserProfile;
   }>
 ) {
   const { user } = props;
+
+  const { logout } = useUserStore();
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenMenu = useCallback((event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  }, []);
+  const handleCloseMenu = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(null);
+    },
+    []
+  );
+
+  const avatarProps = useMemo<Partial<AvatarProps>>(() => {
+    const src = user.images?.[0]?.url;
+    return {
+      alt: user.display_name,
+      src,
+      children: src ? null : user.display_name?.charAt(0) ?? '',
+    };
+  }, [user.display_name, user.images]);
+
   return (
     <Stack direction="row" spacing={1} alignItems="center">
-      {user.images?.length ? (
-        <img alt="User's profile" src={user.images[0].url} height={48} />
-      ) : null}
-      <Stack direction="column" spacing={0}>
-        <Typography>{user?.display_name}</Typography>
-        <Typography>{user?.email}</Typography>
-      </Stack>
+      <Box sx={{ flexGrow: 0 }}>
+        <IconButton onClick={handleOpenMenu} sx={{ p: 0 }}>
+          <Avatar
+            variant="rounded"
+            sx={{ width: '50px', height: '50px' }}
+            {...avatarProps}
+          />
+        </IconButton>
+
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseMenu}
+        >
+          <Stack spacing={0.75} sx={{ px: 2, py: 0.75 }}>
+            <Typography variant="caption">
+              You're signed in as{' '}
+              <Typography component="span" variant="caption" fontWeight={700}>
+                {user.email}
+              </Typography>
+            </Typography>
+          </Stack>
+          <Divider />
+          <MenuItem onClick={logout}>
+            <Typography>Logout</Typography>
+          </MenuItem>
+        </Menu>
+      </Box>
     </Stack>
   );
 }
