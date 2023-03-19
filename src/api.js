3a4fa98bc7be397b908e154b9f623a29294a5374
Dillon Drenzek <dillon@drenzek.com>
2023-03-18 22:59:50 -0700

    tried some new things

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 
 const app = express();
@@ -7,11 +8,15 @@ const router = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
 router.get("/", (req, res) => {
-  res.send('Ok');
+  res.sendFile(path.resolve(__dirname, '../app/public/index.html'));
 });
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
 app.use(`/.netlify/functions/api`, router);
+
+app.use('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../app/public/index.html'));
+});
 
 // Export the app and the serverless function
 module.exports = app;
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import express from 'express';
+import path from 'path';
+import serverless from 'serverless-http';
+// import { queryParser } from 'express-query-parser';
+// import { SpotifyWebApi } from './spotify/spotify-web-api';
+// import { logger } from './middleware/logger';
+// import loadEnv from './env';
+// import api from './api';
+// import auth from './auth';
+
+// const env = loadEnv();
+const app = express();
+// const spotifyWebApi = new SpotifyWebApi(env);
+// const port = env.APP_PORT; // default port to listen
+
+const router = express.Router();
+
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get('/', (req, res) => {
+  res.status(200).send('Ok');
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/index`, router);
+
+// // Log Requests
+// app.use(logger());
+
+// // Query Param parsing
+// app.use(
+//   queryParser({
+//     parseNull: true,
+//     parseUndefined: true,
+//     parseBoolean: true,
+//     parseNumber: true,
+//   })
+// );
+
+// // App API
+// app.use('/api', api(spotifyWebApi));
+
+// // Auth API
+// app.use('/auth', auth(spotifyWebApi));
+
+// Test Endpoint
+app.get('/', (req, res) => {
+  res.status(200).send('Ok');
+});
+
+// Serve Client
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../app/public/index.html'));
+// });
+
+// // start the Express server
+// app.listen(port, () => {
+//   console.log(
+//     [
+//       '******************************',
+//       '       Server started',
+//       '',
+//       `   http://localhost:${port}`,
+//       '*******************************',
+//     ].join('\n')
+//   );
+// });
+
+export default app;
+export const handler = serverless(app);
